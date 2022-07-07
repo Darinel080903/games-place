90341d732e0ed584a788ed25404f5bcd4a8a8867
edualt <eduardosebastianhdz503@gmail.com>
2022-07-06 20:46:11 -0500

    fix(config): wrong sintaxis

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config()
 
 export default{
-    host: procces.env.HOST,
+    host: process.env.HOST,
     database: process.env.DATABASE,
     user: process.env.DB_USER,
     password: process.env.PASSWORD
