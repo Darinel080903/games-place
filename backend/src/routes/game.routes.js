bbae9c440155dc489e37fa225fa10cc6bae96a6e
edualt <eduardosebastianhdz503@gmail.com>
2022-07-11 22:46:34 -0500

    feat(routes): game routes added

--- backend/src/routes/game.routes.js
+++ backend/src/routes/game.routes.js
@@ -4,5 +4,8 @@ import { methods as GameMethods } from '../controllers/game.controller'
 const router = Router();
 
 router.get('/', GameMethods.getGames);
-
+router.post('/', GameMethods.addGame);
+router.get('/:id', GameMethods.getGame);
+router.put('/:id', GameMethods.updateGame);
+router.delete('/:id', GameMethods.deleteGame);
 export default router;
