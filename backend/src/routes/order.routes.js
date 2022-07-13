88e9345f97ac95ac7a82c02889b05bd8986a99eb
edualt <eduardosebastianhdz503@gmail.com>
2022-07-12 20:39:17 -0500

    feat(order.routes): post, put and delete methods routes

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -5,5 +5,8 @@ const router = Router()
 
 router.get('/', OrderMethods.getOrders);
 router.get('/:id', OrderMethods.getOrder);
+router.post('/', OrderMethods.addOrder);
+router.put('/:id', OrderMethods.updateOrder);
+router.delete('/:id', OrderMethods.deleteOrder);
 
 export default router;
