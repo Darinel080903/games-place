e2c31454cdf429de29f1b359b84b0ea56b7d956c
edualt <eduardosebastianhdz503@gmail.com>
2022-07-13 19:56:37 -0500

    feat(database): on delete added

--- backend/src/database/dbsql.sql
+++ backend/src/database/dbsql.sql
@@ -30,7 +30,10 @@ CREATE TABLE user_order(
 	PRIMARY KEY(id),
     CONSTRAINT fk_id
     FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE
 );
+
+INSERT INTO user_order VALUES(1, 1);
 
 CREATE TABLE order_detail(
 	id INT AUTO_INCREMENT,
@@ -39,19 +42,32 @@ CREATE TABLE order_detail(
     quantity INT,
     primary key(id),
     CONSTRAINT fk_order_id
-    FOREIGN KEY (order_id) REFERENCES user_order(id),
+    FOREIGN KEY (order_id) REFERENCES user_order(id)
+    ON DELETE CASCADE,
     CONSTRAINT fk_game_id
     FOREIGN KEY (game_id) REFERENCES game(id)
+    ON DELETE CASCADE
 );
 
 SELECT * FROM users;
 SELECT * FROM game;
 SELECT * FROM user_order;
+SELECT * FROM order_detail;
 
-SELECT user_order.id, user_order.user_id, game.title FROM user_order
-INNER JOIN order_item ON order_item.order_id = user_order.id
-INNER JOIN game ON game.id = order_item.game_id AND user_order.user_id =2;
+SELECT * FROM user_order
+INNER JOIN order_detail ON order_detail.order_id = user_order.id
+INNER JOIN game ON game.id = order_detail.game_id AND user_order.user_id =1;
 
-INSERT INTO user_order VALUES (100, 1), (null,1);
-
+SELECT user_order.id as order_id, user_order.user_id,
+order_detail.id as game_in_order_id,
+order_detail.quantity,
+game.id as game_id,
+game.title as game_title,
+game.game_type,
+game.price,
+game.clasification,
+game.image,
+game.stock FROM user_order
+INNER JOIN order_detail ON order_detail.order_id = user_order.id
+INNER JOIN game ON game.id = order_detail.game_id AND user_order.user_id=1;
 
