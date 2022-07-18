2b3d43b758447cedf089bb789ada3fdae84e357a
edualt <eduardosebastianhdz503@gmail.com>
2022-07-17 23:31:49 -0500

    feat(game_register): form data to api

---
+++ frontend/js/game_register.js
@@ -0,0 +1,38 @@
+const register = document.getElementById('register');
+
+register.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const title = document.getElementById('title').value;
+    const game_type = document.getElementById('game_type').options[document.getElementById('game_type').selectedIndex].value;
+    const price = document.getElementById('price').value;
+    const clasification = document.getElementById('clasification').options[document.getElementById('clasification').selectedIndex].value;
+    const stock = document.getElementById('stock').value;
+    const image = document.getElementById('image').files[0];
+    // const password = document.getElementById('password').value;
+    
+    console.log(image)
+    
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('game_type', game_type);
+    formData.append('price', price);
+    formData.append('clasification', clasification);
+    formData.append('stock', stock);
+    formData.append('image', image);
+
+    fetch('http://localhost:4000/api/games', {
+        method: 'POST',
+        body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        //prevent reloading the page
+        alert('Game added succesfully')
+        // window.location.href = 'index.html';
+    })
+    .catch(err => console.log(err))
+});
+
+
