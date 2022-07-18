41070054421695c56e5939dce0a64ff0ac0298af
edualt <eduardosebastianhdz503@gmail.com>
2022-07-17 23:32:16 -0500

    feat(login): user verify with api

---
+++ frontend/js/login.js
@@ -0,0 +1,30 @@
+const login = document.getElementById('login');
+
+login.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    fetch('http://localhost:4000/api/users')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(user =>  {
+            if(user.email === email && user.password === password) {
+                localStorage.setItem('user', JSON.stringify(user));
+
+                errors.innerHTML = '';
+
+                if(user.is_admin) {
+                    window.location.href = 'registro_juego.html';
+                } else {
+                    window.location.href = 'index.html';
+                }
+            }
+            else {
+                errors.innerHTML = 'Usuario o contraseña incorrectos';
+            }
+                
+        });
+    })
+})
