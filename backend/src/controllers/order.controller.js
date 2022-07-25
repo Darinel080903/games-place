1b462702a7c2763985dbe1a1d6eff6cd75ac87ad
edualt <eduardosebastianhdz503@gmail.com>
2022-07-24 19:22:09 -0500

    feat(order.controller): add deleteItemFromOrderDetail, UpadateItemFromOrderDetailStatus and getOrderByUserAndGameId

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -5,9 +5,11 @@ const getOrders = async (req, res) => {
     try {
         const connection = await getConnection();
         const result = await connection.query('SELECT user_order.id as order_id, user_order.user_id,\
-                                                user_order.unique_id,\
                                                 order_detail.id as game_in_order_id,\
                                                 order_detail.quantity,\
+                                                order_detail.unique_id,\
+                                                order_detail.status,\
+                                                users.email,\
                                                 game.id as game_id,\
                                                 game.title as game_title,\
                                                 game.game_type,\
@@ -16,6 +18,7 @@ const getOrders = async (req, res) => {
                                                 game.image,\
                                                 game.stock FROM user_order\
                                                 INNER JOIN order_detail ON order_detail.order_id = user_order.id\
+                                                INNER JOIN users ON users.id = user_order.user_id\
                                                 INNER JOIN game ON game.id = order_detail.game_id;');
         res.json(result);
     } catch (error) {
@@ -29,7 +32,6 @@ const getOrder = async (req, res) => {
         const { id } = req.params;
         const connection = await getConnection();
         const result = await connection.query('SELECT user_order.id as order_id, user_order.user_id,\
-                                                user_order.unique_id,\
                                                 order_detail.id as game_in_order_id,\
                                                 order_detail.status,\
                                                 order_detail.quantity,\
@@ -63,13 +65,14 @@ const addOrder = async (req, res) => {
 
             console.log(unique_id);
 
-            await connection.query('INSERT INTO user_order SET ?', { user_id: user_id, unique_id: unique_id});
+            await connection.query('INSERT INTO user_order SET ?', { user_id: user_id});
             const order_id = await connection.query('SELECT id FROM user_order WHERE user_id=?', user_id);
-            await connection.query('INSERT INTO order_detail SET ?', { order_id: order_id[0].id, game_id: game_id, quantity: quantity });
+            await connection.query('INSERT INTO order_detail SET ?', { order_id: order_id[0].id, game_id: game_id, quantity: quantity, unique_id: unique_id});
         }else{
+            var unique_id = generateUniqueId({length: 6});
 
-
-            await connection.query('INSERT INTO order_detail SET ?', { order_id: result[0].id, game_id: game_id, quantity: quantity });
+            console.log(unique_id);
+            await connection.query('INSERT INTO order_detail SET ?', { order_id: result[0].id, game_id: game_id, quantity: quantity, unique_id: unique_id});
         }
 
         res.json({ message: 'Order created successfuly' });
@@ -126,6 +129,7 @@ const getOrderByUser = async (req, res) => {
         const connection = await getConnection();
         const result = await connection.query('SELECT user_order.id as order_id, user_order.user_id,\
                                                 order_detail.id as game_in_order_id,\
+                                                order_detail.unique_id,\
                                                 order_detail.quantity,\
                                                 order_detail.status,\
                                                 game.id as game_id,\
@@ -145,6 +149,56 @@ const getOrderByUser = async (req, res) => {
     }
 }
 
+const deleteItemFromOrderDetail = async (req, res) => {
+    try {
+        const { unique_id } = req.body;
+        const connection = await getConnection();
+        await connection.query('DELETE FROM order_detail WHERE unique_id=?', unique_id);
+        res.json({ message: 'Item deleted successfuly' });
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+}
+
+const updateItemFromOrderDetailStatus = async (req, res) => {
+    try {
+        const { unique_id, status } = req.body;
+        const connection = await getConnection();
+        await connection.query('UPDATE order_detail SET ? WHERE unique_id=?', [{ status: status }, unique_id]);
+        res.json({ message: 'Item updated successfuly' });
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+}
+
+const getOrderByUserAndGameId = async (req, res) => {
+    try {
+        const { user_id, game_id } = req.params;
+        const connection = await getConnection();
+        const result = await connection.query('SELECT user_order.id as order_id, user_order.user_id,\
+                                                order_detail.id as game_in_order_id,\
+                                                order_detail.unique_id,\
+                                                order_detail.quantity,\
+                                                order_detail.status,\
+                                                game.id as game_id,\
+                                                game.title as game_title,\
+                                                game.game_type,\
+                                                game.price,\
+                                                game.platform,\
+                                                game.clasification,\
+                                                game.image,\
+                                                game.stock FROM user_order\
+                                                INNER JOIN order_detail ON order_detail.order_id = user_order.id\
+                                                INNER JOIN game ON game.id = order_detail.game_id AND user_order.user_id=? AND order_detail.game_id=? AND order_detail.status="Disponible"', [user_id, game_id]);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+}
+
 
 export const methods = {
     getOrders,
@@ -153,5 +207,8 @@ export const methods = {
     updateOrder,
     deleteOrder,
     getOrderByUser,
-    updateOrderDetail
+    updateOrderDetail,
+    deleteItemFromOrderDetail,
+    updateItemFromOrderDetailStatus,
+    getOrderByUserAndGameId
 }
